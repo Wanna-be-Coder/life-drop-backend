74856a40f88060a047fafb47a7173fe57cd9ceda
samin-sharar-val <samin@valerianfunds.com>
2024-09-20 06:07:19 +0600

    Logger middleware added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfileModule } from './profile/profile.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { ProfileController } from './profile/profile.controller';
 
 @Module({
   imports: [ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(ProfileController);
+  }
+}
